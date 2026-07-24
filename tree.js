cb967bfd6295ab3cd738790dbbb96ee9f9a915b0
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-07-24 21:25:23 +0800

    add tree.js file

---
+++ tree.js
@@ -0,0 +1,8 @@
+// tree class & build tree function
+class Tree {
+    // constructor
+    constructor(arr) {
+        this.arr = arr;
+        // this.root = buidTree(sortedArr, 0, sortedArr.length - 1)        
+    };
+};

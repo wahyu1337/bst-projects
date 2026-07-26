2db3e75c001c3b0161ae5e990cd7c2f9d188779c
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-07-26 21:25:03 +0800

    ADD & UPDATE few method
    Finishing Includes()
    Finishing sort() method
    
    Sort method return with sorted
    and deduplicate the array

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ import Tree from "./tree.js";
 import { prettyPrint as logs } from "./pretty.js";
 
 let node = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
-logs(node.root)
+node.sort();
+logs(node.root);
+console.log(node.includes(7));
--- tree.js
+++ tree.js
@@ -2,9 +2,13 @@ import Node from "./node.js";
 
 // tree class & build tree function
 export default class Tree {
+    // private
+    #array
     // constructor
     constructor(arr) {
-        this.root = this.buildTree(arr, 0, arr.length - 1);
+        this.#array = arr;
+        this.sortedArr = this.sort(arr)
+        this.root = this.buildTree(this.sortedArr, 0, this.sortedArr.length - 1);
     };
 
     // building a tree method
@@ -21,5 +25,30 @@ export default class Tree {
         root.right = this.buildTree(arr, mid + 1, end);
 
         return root;
+    };
+
+    // sort() & duplicate using set() method for sorted an array and
+    // remove duplicate
+    sort() {
+        // sorted the array using built in method
+        return [...new Set(this.#array)].sort((a, b) => a - b);
+    }
+
+    // includes(value) method to check matching value
+    includes(value) {
+        // get the root into var
+        let curr = this.root;
+
+        while (curr !== null) {
+            // if node is match
+            if (curr.data === value) {
+                return true;
+            } //search in right subtree
+            else if (curr.data < value) {
+                curr = curr.right;
+            } else {
+                curr = curr.left; // search in left subtree
+            }
+        } return false; // if values not found
     };
 };
